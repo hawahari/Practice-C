488681974a8ba339ec5c94e4d8b0a30f9fc1aacc
hawahari <149294262+hawahari@users.noreply.github.com>
2024-01-12 23:48:10 +0545

    Update Str.c

--- Str.c
+++ Str.c
@@ -5,10 +5,15 @@ const char *name;
 int age;
 int weight;
 };
+typedef struct cell{
+const char *nameof;
+const char *processor;
+int size;
+}phone;
 
 int main()
 {
 struct fish f={"Hawa",19,100};
-
+phone p={"Samsung","ARM",15};
 return 0;
 }
